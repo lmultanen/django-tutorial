868a9f7e5d8a97b2bc2ec3714204366c44759a6f
Luke Multanen <lmultanen.uchicago@gmail.com>
2023-01-25 14:42:25 -0600

    added template for question detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -13,8 +13,19 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return HttpResponse(template.render(context,request))
 
+    # shortcut using render:
+    # return render(request, 'polls/index.html', context)
+
 def detail(request, question_id):
-    return HttpResponse("You are viewing question %s." %question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+    # shortcut using get_object_or_404
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("You are viewing the results of question %s." %question_id)
