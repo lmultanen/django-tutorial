374281b1f720dc8c58050e442bd5f3f2db42799e
Luke Multanen <lmultanen.uchicago@gmail.com>
2023-01-25 14:11:38 -0600

    added new polls app from django tutorial to get familiar with db models; will continue working through this tutorial and then look to make a new project

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstapp'
+    'firstapp',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
